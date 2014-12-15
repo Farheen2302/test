9a8be3929d1a60d16797599a6947a5e9819d1c31
aakash <aakash.srt@gmail.com>
2014-12-15 22:49:02 +0530

    Aakash here

---
+++ xyz.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+int comp(int *a ,int *b)
+{
+
+    if(*a>*b)
+        return 1;
+    return 0;
+}
+int main()
+{
+    int arr[2000],size,l,s;
+    int register i;
+    long long register count;
+    scanf("%d",&size);
+    while(size>0)
+    {
+        count = 0;
+        for(i=0;i<size;i++)
+            scanf("%d",arr+i);
+
+        qsort(arr,size,sizeof(int),comp);
+
+        for(i=size-1;i>1;i--)
+        {
+            l=i-1;
+            s=0;
+            while(s<l)
+            {
+                if(arr[s]+arr[l]<arr[i])
+                {
+                    count += l-s;
+                    s++;
+                }
+                else
+                    l--;
+            }
+
+        }
+        printf("%lld\n",count);
+        scanf("%d",&size);
+    }
+    return 0;
+}
